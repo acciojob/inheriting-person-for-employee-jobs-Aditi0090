deeb27bcaf632abcaa43f8377e372ac2ccca324c
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-03-03 15:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
   console.log(
-    `Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
 };
 
 // Do not change code below this line
